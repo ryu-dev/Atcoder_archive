720452256af92c304ebbe2ac50d87b7a9bedacb6
watari_eng <watari.dev.eng@gmail.com>
2020-04-12 21:17:13 +0900

    [AC] abc162 abc162_a

---
+++ atcoder.jp/abc162/abc162_a/Main.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string n;cin>>n;
+  if(n[0]=='7' || n[1]=='7' || n[2]=='7'){
+    cout<<"Yes"<<endl;
+  }else{
+    cout<<"No"<<endl;
+  }
+}
