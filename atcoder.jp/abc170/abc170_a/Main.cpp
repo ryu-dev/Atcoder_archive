3758c08ca734ac5acfef776c564fcc58fc64fe63
watari_eng <watari.dev.eng@gmail.com>
2020-06-25 22:26:38 +0900

    [AC] abc170 abc170_a

---
+++ atcoder.jp/abc170/abc170_a/Main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    for(int i=0;i<5;++i){
+        cin>>x;
+        if(x==0){
+            cout<<i+1<<endl;
+            return 0;
+        }
+    }
+}
