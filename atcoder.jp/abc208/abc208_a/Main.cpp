35d6eaf18738eddc1b03e1e1fead14783b6fcdab
watari_eng <watari.dev.eng@gmail.com>
2021-07-04 21:11:59 +0900

    [AC] abc208 abc208_a

---
+++ atcoder.jp/abc208/abc208_a/Main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a,b;cin>>a>>b;
+	if(a>b){
+      cout<<"No"<<endl;
+    }else if(a*6>=b){
+      cout<<"Yes"<<endl;
+    }else{
+      cout<<"No"<<endl;
+    }
+}
