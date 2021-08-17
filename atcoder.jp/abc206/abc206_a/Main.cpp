f6118b07c0b34095be1c941deac0927c9c0101dc
watari_eng <watari.dev.eng@gmail.com>
2021-08-17 22:48:18 +0900

    [AC] abc206 abc206_a

---
+++ atcoder.jp/abc206/abc206_a/Main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n;cin>>n;
+  if((n*108/100)<206){
+    cout<<"Yay!"<<endl;
+  }else if((n*108/100)==206){
+    cout<<"so-so"<<endl;
+  }else{
+    cout<<":("<<endl;
+  }
+}
